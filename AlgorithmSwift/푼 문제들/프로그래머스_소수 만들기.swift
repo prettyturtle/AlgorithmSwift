5b5a0338ffed14866443999fe7a3b1d8b2836718
prettyturtle <unifconti@gmail.com>
2022-03-07 15:08:23 +0900

    프로그래머스_소수 만들기 문제풀이

---
+++ AlgorithmSwift/푼 문제들/프로그래머스_소수 만들기.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+func isPrime(_ n: Int) -> Bool {
+    for i in 2...Int(sqrt(Double(n))) {
+        if n % i == 0 {
+            return false
+        }
+    }
+    return true
+}
+
+func solution(_ nums:[Int]) -> Int {
+    var answer = 0
+
+    for i in 0..<nums.count-2 {
+        for j in i+1..<nums.count-1 {
+            for k in j+1..<nums.count {
+                let number = nums[i] + nums[j] + nums[k]
+                if isPrime(number) {
+                    answer += 1
+                }
+            }
+        }
+    }
+
+    return answer
+}
