1d73374959e421688828c445cfad7b6b5570b487
prettyturtle <unifconti@gmail.com>
2022-03-28 03:06:47 +0900

    프로그래머스_삼각 달팽이 문제풀이

---
+++ AlgorithmSwift/푼 문제들/프로그래머스_삼각 달팽이.swift
@@ -0,0 +1,77 @@
+import Foundation
+
+enum Direction: String {
+    case up
+    case down
+    case straight
+}
+
+func solution(_ n: Int) -> [Int] {
+    
+    var snailArr = [[[Int]]]()
+    
+    for _ in 0...n {
+        let temp = [Int]()
+        snailArr.append([temp, temp])
+    }
+    
+    var direction: Direction = .down
+    
+    var number = 1
+    var step = 1
+    var count = step
+    var side = n
+    
+    while true {
+        if number > n * (n+1) / 2 {
+            break
+        }
+        switch direction {
+        case .up:
+            if count <= step {
+                direction = .down
+                step += 2
+                count = step
+                side -= 1
+            }
+        case .down:
+            if count >= side {
+                direction = .straight
+                count = step
+            }
+        case .straight:
+            if count >= side {
+                direction = .up
+                count = side
+            }
+        }
+        
+        switch direction {
+        case .up:
+            snailArr[count][1].insert(number, at: 0)
+            number += 1
+            count -= 1
+        case .down:
+            snailArr[count][0].append(number)
+            number += 1
+            count += 1
+        case .straight:
+            snailArr[side][0].append(number)
+            number += 1
+            count += 1
+        }
+    }
+    
+    var ret = [Int]()
+    
+    for i in snailArr {
+        for j in i[0] { ret.append(j) }
+        for j in i[1] { ret.append(j) }
+    }
+    
+    return ret
+}
+
+print(solution(4) == [1,2,9,3,10,8,4,5,6,7])
+print(solution(5) == [1,2,12,3,13,11,4,14,15,10,5,6,7,8,9])
+print(solution(6) == [1,2,15,3,16,14,4,17,21,13,5,18,19,20,12,6,7,8,9,10,11])
