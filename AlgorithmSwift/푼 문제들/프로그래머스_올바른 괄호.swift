c1107f43d52400131ca8f3a0120f55711349c8f3
prettyturtle <unifconti@gmail.com>
2022-04-21 09:37:33 +0900

    프로그래머스_올바른 괄호 문제풀이

--- AlgorithmSwift/푼 문제들/프로그래머스_올바른 괄호.swift
+++ AlgorithmSwift/푼 문제들/프로그래머스_올바른 괄호.swift
@@ -1,8 +1,31 @@
-//
-//  프로그래머스_올바른 괄호.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/04/05.
-//
-
 import Foundation
+
+func solution(_ s: String) -> Bool {
+    var ans = false
+
+    let rightQueue = s.map { String($0) }
+    if rightQueue.first == ")" || rightQueue.count == 1 {
+        return false
+    }
+    
+    var count = 0
+    for i in rightQueue {
+        if count == 0 && i == ")" {
+            return false
+        }
+        
+        if i == "(" {
+            count += 1
+        } else {
+            count -= 1
+        }
+    }
+    if count == 0 { ans = true }
+    return ans
+}
+
+print(solution("()()") == true)
+print(solution("(())()") == true)
+print(solution(")()(") == false)
+print(solution("(()(") == false)
+print(solution("())(") == false)
