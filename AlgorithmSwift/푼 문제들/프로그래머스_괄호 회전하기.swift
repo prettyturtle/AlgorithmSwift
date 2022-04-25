d2cb480b9ca9d2fb741baabc483d499a11230aaf
prettyturtle <unifconti@gmail.com>
2022-04-25 21:51:53 +0900

    프로그래머스_괄호 회전하기 문제풀이

--- AlgorithmSwift/푼 문제들/프로그래머스_괄호 회전하기.swift
+++ AlgorithmSwift/푼 문제들/프로그래머스_괄호 회전하기.swift
@@ -1,8 +1,44 @@
-//
-//  프로그래머스_괄호 회전하기.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/04/10.
-//
-
 import Foundation
+
+func isRight(_ arr: [String]) -> Bool {
+    var leftStack = [String]()
+    for i in arr {
+        if leftStack.isEmpty { leftStack.append(i) }
+        else {
+            switch i {
+            case ")":
+                if leftStack.last == "(" { leftStack.removeLast() }
+                else { leftStack.append(i) }
+            case "}":
+                if leftStack.last == "{" { leftStack.removeLast() }
+                else { leftStack.append(i) }
+            case "]":
+                if leftStack.last == "[" { leftStack.removeLast() }
+                else { leftStack.append(i) }
+            default:
+                leftStack.append(i)
+            }
+        }
+    }
+    return leftStack.isEmpty ? true : false
+}
+func solution(_ s: String) -> Int {
+    var ret = 0
+    var count = s.count
+    var arrS = s.map { String($0) }
+    
+    while count > 0 {
+        count -= 1
+        if isRight(arrS) {
+            ret += 1
+        }
+        let first = arrS.removeFirst()
+        arrS.append(first)
+    }
+    return ret
+}
+
+print(solution("[](){}") == 3)
+print(solution("}]()[{") == 2)
+print(solution("[)(]") == 0)
+print(solution("}}}") == 0)
