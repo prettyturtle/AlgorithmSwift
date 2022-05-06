77cf8431a56d7011e7ce9932e182dfd6cfd24a5b
prettyturtle <unifconti@gmail.com>
2022-05-06 20:06:31 +0900

    RGB거리 문제풀이

--- AlgorithmSwift/푼 문제들/RGB거리.swift
+++ AlgorithmSwift/푼 문제들/RGB거리.swift
@@ -1,8 +1,18 @@
-//
-//  RGB거리.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/05/02.
-//
+let N = Int(readLine()!)!
 
-import Foundation
+var houses = [[Int]]()
+
+for _ in 1...N {
+    let house = readLine()!.split(separator: " ").map { Int($0)! }
+    houses.append(house)
+}
+
+var dp = Array(repeating: [0, 0, 0], count: N+1)
+
+for i in 1...N {
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + houses[i - 1][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + houses[i - 1][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + houses[i - 1][2]
+}
+
+print(dp.last!.min()!)
