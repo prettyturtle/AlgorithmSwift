dc8d22e73bc849e21fdd2cb71de58a5569e6acfd
prettyturtle <unifconti@gmail.com>
2022-05-11 07:07:13 +0900

    프로그래머스_카펫 문제풀이

--- AlgorithmSwift/푼 문제들/프로그래머스_카펫.swift
+++ AlgorithmSwift/푼 문제들/프로그래머스_카펫.swift
@@ -1,8 +1,36 @@
-//
-//  프로그래머스_카펫.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/04/13.
-//
-
 import Foundation
+
+func solution(_ brown: Int, _ yellow: Int) -> [Int] {
+    
+    let area = brown + yellow
+    
+    var li = [[Int]]()
+    
+    for i in 3...area {
+        if area % i == 0 {
+            if li.contains([max(i, area / i), min(i, area / i)]) { break }
+            li.append([max(i, area / i), min(i, area / i)])
+        }
+    }
+    if li.count == 1 {
+        return li.first!
+    }
+    for i in li {
+        if findSize(arr: i).0 == brown && findSize(arr: i).1 == yellow {
+            return i
+        }
+    }
+
+    return []
+}
+
+func findSize(arr: [Int]) -> (Int, Int) {
+    let brown = arr[0] * 2 + (arr[1] - 2) * 2
+    let yellow = (arr[0] * arr[1]) - brown
+    let ret = (brown, yellow)
+    return ret
+}
+
+print(solution(10, 2) == [4, 3])
+print(solution(8, 1) == [3, 3])
+print(solution(24, 24) == [8, 6])
