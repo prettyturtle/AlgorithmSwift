1f6c91054086d7193b46b8ca4455f381174d829b
prettyturtle <unifconti@gmail.com>
2022-05-12 00:02:24 +0900

    프로그래머스_최소직사각형 문제풀이

--- AlgorithmSwift/푼 문제들/프로그래머스_최소직사각형.swift
+++ AlgorithmSwift/푼 문제들/프로그래머스_최소직사각형.swift
@@ -1,8 +1,26 @@
-//
-//  프로그래머스_최소직사각형.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/04/16.
-//
-
 import Foundation
+
+// func solution(_ sizes: [[Int]]) -> Int {
+//     let sortedSizes = sizes.map { $0.sorted() }
+//     var maxHeight = 0
+//     var maxWidth = 0
+    
+//     for size in sortedSizes {
+//         maxWidth = max(maxWidth, size[0])
+//         maxHeight = max(maxHeight, size[1])
+//     }
+    
+//     return maxWidth * maxHeight
+// }
+
+func solution(_ sizes: [[Int]]) -> Int {
+    var maxHeight = 0
+    var maxWidth = 0
+    
+    for size in sizes {
+        maxWidth = max(maxWidth, size.max()!)
+        maxHeight = max(maxHeight, size.min()!)
+    }
+    
+    return maxWidth * maxHeight
+}
