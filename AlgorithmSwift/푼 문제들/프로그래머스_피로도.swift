acd6c31376a8f99e45a9268d7a5472a343f4df17
prettyturtle <unifconti@gmail.com>
2022-05-14 22:50:04 +0900

    프로그래머스_피로도 문제풀이

--- AlgorithmSwift/푼 문제들/프로그래머스_피로도.swift
+++ AlgorithmSwift/푼 문제들/프로그래머스_피로도.swift
@@ -1,8 +1,54 @@
-//
-//  프로그래머스_피로도.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/04/19.
-//
-
 import Foundation
+
+func allCase(_ n: Int) -> [[Int]] {
+    
+    var ret = [[Int]]()
+    let li = Array((1...n))
+    
+    func aa(current: [Int], numLi: [Int]) {
+        if current.count == n {
+            ret.append(current)
+        }
+        
+        for i in numLi.indices {
+            let newCurrent = current + [numLi[i]]
+            var newLi = numLi
+            newLi.remove(at: i)
+            aa(current: newCurrent, numLi: newLi)
+        }
+    }
+    
+    aa(current: [], numLi: li)
+    
+    for i in ret.indices {
+        for j in ret[i].indices {
+            ret[i][j] -= 1
+        }
+    }
+    
+    return ret
+}
+
+
+
+func solution(_ k: Int, _ dungeons: [[Int]]) -> Int {
+    let all = allCase(dungeons.count)
+    
+    var ret = [Int]()
+    
+    for i in all {
+        var count = 0
+        var k = k
+        for j in i {
+            if k >= dungeons[j][0] {
+                count += 1
+                k -= dungeons[j][1]
+            }
+        }
+        ret.append(count)
+    }
+    return ret.max()!
+}
+
+print(solution(80, [[80,20], [50,40], [30,10]]) == 3)
+print(solution(40, [[40, 20], [10, 10], [10, 10], [10, 10], [10, 10]]) == 4)
