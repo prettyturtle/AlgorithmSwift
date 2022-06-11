bb3e16d8c521a47e04601100e3ba97d1ebfd68b3
prettyturtle <unifconti@gmail.com>
2022-06-11 22:28:45 +0900

    프로그래머스_콜라츠 추측 문제풀이

--- AlgorithmSwift/푼 문제들/프로그래머스_콜라츠 추측.swift
+++ AlgorithmSwift/푼 문제들/프로그래머스_콜라츠 추측.swift
@@ -1,8 +1,25 @@
-//
-//  프로그래머스_콜라츠 추측.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/06/09.
-//
+func solution(_ num: Int) -> Int {
+    return aaa(num, 0)
+}
 
-import Foundation
+func aaa(_ num: Int, _ count: Int) -> Int {
+    if count >= 501 && num != 1 {
+        return -1
+    }
+    
+    if num == 1 {
+        return count
+    }
+    
+    var temp = num
+    
+    if num % 2 == 0 {
+        temp = num / 2
+    }
+    
+    if num % 2 == 1 {
+        temp = num * 3 + 1
+    }
+    
+    return aaa(temp, count + 1)
+}
