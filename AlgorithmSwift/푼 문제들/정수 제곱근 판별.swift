c40dfa0b4c344fdc33fcfd600e9259b292ab3f30
prettyturtle <unifconti@gmail.com>
2022-06-12 22:32:55 +0900

    정수 제곱근 판별 문제풀이

--- AlgorithmSwift/푼 문제들/정수 제곱근 판별.swift
+++ AlgorithmSwift/푼 문제들/정수 제곱근 판별.swift
@@ -1,8 +1,17 @@
-//
-//  정수 제곱근 판별.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/06/09.
-//
-
-import Foundation
+func solution(_ n: Int64) -> Int64 {
+    let nn = Int64(n)
+    var x: Int64 = 0
+    
+    for i in 1...nn {
+        if i * i == nn {
+            x = i
+            break
+        }
+    }
+    
+    if x == 0 {
+        return -1
+    } else {
+        return Int64((x+1) * (x+1))
+    }
+}
