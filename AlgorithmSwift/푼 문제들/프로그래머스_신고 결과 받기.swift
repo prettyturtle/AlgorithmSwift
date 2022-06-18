4a8bffea1933801bffe15b0962ab7791eb401e39
prettyturtle <unifconti@gmail.com>
2022-06-18 19:05:28 +0900

    프로그래머스_신고 결과 받기 다시풀기

--- AlgorithmSwift/푼 문제들/프로그래머스_신고 결과 받기.swift
+++ AlgorithmSwift/푼 문제들/프로그래머스_신고 결과 받기.swift
@@ -45,3 +45,44 @@ func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
     
     return returnArray
 }
+
+
+// 재풀이
+import Foundation
+
+func solution(_ id_list: [String], _ report: [String], _ k: Int) -> [Int] {
+    
+    let setReport = Set(report).map { $0.split(separator: " ").map { String($0) } }
+    var reportDict = [String: [String]]()
+    
+    for i in setReport {
+        if reportDict[i[1]] == nil {
+           reportDict[i[1]] = [i[0]]
+        } else {
+            reportDict[i[1]]!.append(i[0])
+        }
+    }
+        
+    var mailDict = [String: Int]()
+    
+    for i in id_list {
+        mailDict[i] = 0
+    }
+    
+    for value in reportDict.values {
+        if value.count >= k {
+            for i in value {
+                mailDict[i]! += 1
+            }
+        }
+    }
+    
+    var ret = [Int]()
+    
+    for i in id_list {
+        ret.append(mailDict[i]!)
+    }
+    
+    return ret
+}
+// https://velog.io/@yc1303/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-Lv.1-%EC%8B%A0%EA%B3%A0-%EA%B2%B0%EA%B3%BC-%EB%B0%9B%EA%B8%B0-Swift-%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4
