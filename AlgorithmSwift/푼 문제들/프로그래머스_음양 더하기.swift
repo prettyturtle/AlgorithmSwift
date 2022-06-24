5d68aa34e3e1ee5d4b260c578169aa026895da9f
prettyturtle <unifconti@gmail.com>
2022-06-24 21:47:00 +0900

    프로그래머스_음양 더하기 문제풀이

--- AlgorithmSwift/푼 문제들/프로그래머스_음양 더하기.swift
+++ AlgorithmSwift/푼 문제들/프로그래머스_음양 더하기.swift
@@ -1,8 +1,32 @@
-//
-//  프로그래머스_음양 더하기.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/06/23.
-//
-
 import Foundation
+
+func solution(_ absolutes: [Int], _ signs: [Bool]) -> Int {
+    
+    var count = 0
+    
+    for i in 0..<signs.count {
+        if signs[i] { count += absolutes[i] }
+        else { count -= absolutes[i] }
+    }
+    
+    return count
+}
+
+
+// 이전 풀이
+func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
+
+    var absolutes = absolutes
+    var signs = signs
+
+    for i in 0..<absolutes.count {
+        if signs[i] == false {
+            absolutes[i] *= -1
+        }
+    }
+
+    let result = absolutes.reduce(0, +)
+
+    return result
+}
+
