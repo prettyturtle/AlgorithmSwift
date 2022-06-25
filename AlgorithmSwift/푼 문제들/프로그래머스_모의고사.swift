b4dbca80a330102f4c268023c8590def309a9ae6
prettyturtle <unifconti@gmail.com>
2022-06-25 23:00:12 +0900

    프로그래머스_모의고사 재풀이

--- AlgorithmSwift/푼 문제들/프로그래머스_모의고사.swift
+++ AlgorithmSwift/푼 문제들/프로그래머스_모의고사.swift
@@ -30,3 +30,37 @@ func solution(_ answers:[Int]) -> [Int] {
     
     return returnArray
 }
+
+
+import Foundation
+
+func solution(_ answers: [Int]) -> [Int] {
+    
+    let f = [1,2,3,4,5]
+    let s = [2,1,2,3,2,4,2,5]
+    let t = [3,3,1,1,2,2,4,4,5,5]
+    
+    var count = [0,0,0]
+    
+    for i in 0..<answers.count {
+        if answers[i] == f[i % f.count] {
+            count[0] += 1
+        }
+        if answers[i] == s[i % s.count] {
+            count[1] += 1
+        }
+        if answers[i] == t[i % t.count] {
+            count[2] += 1
+        }
+    }
+    
+    var result = [Int]()
+    
+    for i in 0..<count.count {
+        if count[i] == count.max() {
+            result.append(i+1)
+        }
+    }
+    
+    return result
+}
