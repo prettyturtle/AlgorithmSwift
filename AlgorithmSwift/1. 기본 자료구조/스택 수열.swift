431e53584ab5ae8c6aa0257a55f74ecd2b267d35
prettyturtle <unifconti@gmail.com>
2022-06-29 22:03:29 +0900

    스택 수열 재풀이

--- AlgorithmSwift/1. 기본 자료구조/스택 수열.swift
+++ AlgorithmSwift/1. 기본 자료구조/스택 수열.swift
@@ -25,3 +25,30 @@ for _ in 0..<n {
 }
 
 print(answer.joined(separator: "\n"))
+
+
+// 재풀이
+let n = Int(readLine()!)!
+
+var count = 1
+var stack = [Int]()
+var result = [String]()
+
+for _ in 0..<n {
+    let num = Int(readLine()!)!
+    
+    while count <= num {
+        stack.append(count)
+        result.append("+")
+        count += 1
+    }
+    
+    if stack.last == num {
+        stack.removeLast()
+        result.append("-")
+    } else {
+        print("NO")
+        exit(0)
+    }
+}
+result.forEach { print($0) }
