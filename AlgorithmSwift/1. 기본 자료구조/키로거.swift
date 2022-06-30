3853663e97d7c1d7e370ba6694aa206d5b878866
prettyturtle <unifconti@gmail.com>
2022-07-01 00:09:48 +0900

    키로거 재풀이

--- AlgorithmSwift/1. 기본 자료구조/키로거.swift
+++ AlgorithmSwift/1. 기본 자료구조/키로거.swift
@@ -27,3 +27,33 @@ for _ in 1...testCase {
     leftStack.append(contentsOf: rightStack)
     print(leftStack.joined(separator: ""))
 }
+
+
+// 재풀이
+let t = Int(readLine()!)!
+
+for _ in 1...t {
+    let keys = readLine()!.map { String($0) }
+    
+    var lstack = [String]()
+    var rstack = [String]()
+    
+    for i in keys {
+        if i == "<" {
+            if !lstack.isEmpty {
+                rstack.append(lstack.removeLast())
+            }
+        } else if i == ">" {
+            if !rstack.isEmpty {
+                lstack.append(rstack.removeLast())
+            }
+        } else if i == "-" {
+            if !lstack.isEmpty {
+                lstack.removeLast()
+            }
+        } else {
+            lstack.append(i)
+        }
+    }
+    print((lstack + rstack.reversed()).joined())
+}
