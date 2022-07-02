837cf8164e200af0d34542c3e7aadc0be145a759
prettyturtle <unifconti@gmail.com>
2022-07-02 21:50:58 +0900

    나이순 정렬 재풀이

--- AlgorithmSwift/3. 기본 정렬 알고리즘/나이순 정렬.swift
+++ AlgorithmSwift/3. 기본 정렬 알고리즘/나이순 정렬.swift
@@ -20,3 +20,24 @@ let sortedMembers = members.sorted {
 for member in sortedMembers {
     print(member.1, member.2)
 }
+
+
+
+// 재풀이
+let t = Int(readLine()!)!
+
+var people = [(Int, String)]()
+
+for _ in 1...t {
+    let p = readLine()!.split(separator: " ")
+    let a = Int(p[0])!
+    let b = String(p[1])
+    
+    people.append((a, b))
+}
+
+people.sort {
+    $0.0 < $1.0
+}
+
+people.forEach { print($0.0, $0.1) }
