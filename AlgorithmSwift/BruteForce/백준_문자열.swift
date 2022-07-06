f4acebe5144416e4837d205220703ce503928312
prettyturtle <unifconti@gmail.com>
2022-07-06 22:13:41 +0900

    백준_문자열 문제풀이

--- AlgorithmSwift/BruteForce/백준_문자열.swift
+++ AlgorithmSwift/BruteForce/백준_문자열.swift
@@ -1,8 +1,31 @@
-//
-//  백준_문자열.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/07/04.
-//
+let ab = readLine()!.split(separator: " ").map { String($0) }
+let a = ab[0]
+let b = ab[1]
 
-import Foundation
+let ac = a.count
+let bc = b.count
+
+let aArr = a.map { String($0) }
+let bArr = b.map { String($0) }
+
+func hansu(_ x: [String], _ y: [String]) -> Int {
+    var count = 0
+    
+    for i in 0..<x.count {
+        if x[i] != y[i] {
+            count += 1
+        }
+    }
+    
+    return count
+}
+
+var minV = 50
+
+for i in 0...abs(ac-bc) {
+    let x = aArr
+    let y = bArr[i...].map { String($0) }
+    minV = min(minV, hansu(x, y))
+}
+
+print(minV)
