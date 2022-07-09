051666b0223035714151c375c120da906db51b7c
prettyturtle <unifconti@gmail.com>
2022-07-09 21:07:08 +0900

    백준_콘테스트 문제풀이

--- AlgorithmSwift/SortApplication/백준_콘테스트.swift
+++ AlgorithmSwift/SortApplication/백준_콘테스트.swift
@@ -1,8 +1,16 @@
-//
-//  백준_콘테스트.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/07/04.
-//
+var w = [Int]()
+var k = [Int]()
+for _ in 1...10 {
+    w.append(Int(readLine()!)!)
+}
+for _ in 1...10 {
+    k.append(Int(readLine()!)!)
+}
 
-import Foundation
+w.sort()
+k.sort()
+
+let wm = w[9] + w[8] + w[7]
+let km = k[9] + k[8] + k[7]
+
+print(wm, km)
