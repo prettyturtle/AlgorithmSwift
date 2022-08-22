4fa7cf10b302f181bee7702ecc7ffb55bfc0fc3d
prettyturtle <unifconti@gmail.com>
2022-08-23 06:40:00 +0900

    백준_점프 점프 문제풀이

---
+++ AlgorithmSwift/푼 문제들/2차원 배열의 합.swift
@@ -0,0 +1,8 @@
+//
+//  2차원 배열의 합.swift
+//  AlgorithmSwift
+//
+//  Created by yc on 2022/08/22.
+//
+
+import Foundation
--- AlgorithmSwift/푼 문제들/점프 점프.swift
+++ AlgorithmSwift/푼 문제들/점프 점프.swift
@@ -1,8 +1,34 @@
-//
-//  점프 점프.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/08/21.
-//
+let xyp1p2 = readLine()!.split(separator: " ").map { Int($0)! }
 
-import Foundation
+let x = xyp1p2[0]
+let y = xyp1p2[1]
+var p1 = xyp1p2[2]
+var p2 = xyp1p2[3]
+
+var setNums = Set<Int>()
+var setCount = setNums.count
+
+while true {
+    if setNums.count > 1000000 {
+        print(-1)
+        break
+    }
+    
+    setNums.insert(p1)
+    if setCount == setNums.count {
+        print(p1)
+        break
+    } else {
+        setCount = setNums.count
+    }
+    setNums.insert(p2)
+    if setCount == setNums.count {
+        print(p2)
+        break
+    } else {
+        setCount = setNums.count
+    }
+    
+    p1 += x
+    p2 += y
+}
