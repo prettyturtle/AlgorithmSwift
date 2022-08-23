2d30cfa4fb3f0e3cd958ea1f1a7126c5ff96cc24
prettyturtle <unifconti@gmail.com>
2022-08-24 06:48:09 +0900

    백준_2차원 배열의 합 문제풀이

--- AlgorithmSwift/푼 문제들/2차원 배열의 합.swift
+++ AlgorithmSwift/푼 문제들/2차원 배열의 합.swift
@@ -1,8 +1,39 @@
-//
-//  2차원 배열의 합.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/08/22.
-//
+extension String {
+    var asInt: Int { return Int(self)! }
+    var asIntArr: [Int] { return self.split(separator: " ").map { Int($0)! } }
+}
 
-import Foundation
+let nm = readLine()!.asIntArr
+let (n, m) = (nm[0], nm[1])
+var nums = [[Int]]()
+
+for _ in 1...n {
+    nums.append(readLine()!.asIntArr)
+}
+
+var dp = Array(
+    repeating: Array(
+        repeating: 0,
+        count: m + 1
+    ),
+    count: n + 1
+)
+
+for i in 1...n {
+    for j in 1...m {
+        dp[i][j] = dp[i - 1][j]
+                    + dp[i][j - 1]
+                    - dp[i - 1][j - 1]
+                    + nums[i - 1][j - 1]
+        // 위쪽 누적합 + 옆쪽 누적합 - 대각선 누적합 + 현재 값
+    }
+}
+
+let k = readLine()!.asInt
+
+for _ in 1...k {
+    let ijxy = readLine()!.asIntArr
+    let (i, j, x, y) = (ijxy[0], ijxy[1], ijxy[2], ijxy[3])
+    
+    print(dp[x][y] - dp[x][j - 1] - dp[i - 1][y] + dp[i - 1][j - 1])
+}
---
+++ AlgorithmSwift/푼 문제들/연속합.swift
@@ -0,0 +1,8 @@
+//
+//  연속합.swift
+//  AlgorithmSwift
+//
+//  Created by yc on 2022/08/23.
+//
+
+import Foundation
