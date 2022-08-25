3d7d7a7ce992cc64a8b553494f0070647532cc74
prettyturtle <unifconti@gmail.com>
2022-08-26 06:40:13 +0900

    백준_요세푸스 문제 0 문제풀이

---
+++ AlgorithmSwift/푼 문제들/시험 감독.swift
@@ -0,0 +1,8 @@
+//
+//  시험 감독.swift
+//  AlgorithmSwift
+//
+//  Created by yc on 2022/08/25.
+//
+
+import Foundation
--- AlgorithmSwift/푼 문제들/요세푸스 문제 0.swift
+++ AlgorithmSwift/푼 문제들/요세푸스 문제 0.swift
@@ -1,8 +1,38 @@
-//
-//  요세푸스 문제 0.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/08/24.
-//
+extension String {
+    var asInt: Int { return Int(self)! }
+    var asIntArr: [Int] { return self.split(separator: " ").map { Int($0)! } }
+}
 
-import Foundation
+
+//[1,2,3,4,5,6,7]
+//[3,4,5,6,7,1,2,]
+//[4,5,6,7,1,2,]
+//[6,7,1,2,4,5,]
+//[7,1,2,4,5,]
+//[2,4,5,7,1,]
+//[4,5,7,1,]
+//[7,1,4,5,]
+//[1,4,5,]
+//[5,1,4,]
+
+let nk = readLine()!.asIntArr
+let (n, k) = (nk[0], nk[1])
+
+var numArr = (1...n).map { Int($0) }
+
+var result = [Int]()
+
+while !numArr.isEmpty {
+    
+    for _ in 1..<k {
+        numArr.append(numArr.removeFirst())
+    }
+    
+    result.append(numArr.removeFirst())
+}
+
+var answer = result.map { String($0) }.joined(separator: ", ")
+
+answer = "<" + answer
+answer += ">"
+print(answer)
