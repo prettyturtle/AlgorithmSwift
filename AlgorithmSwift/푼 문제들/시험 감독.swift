d58021141048a978c97d0362fff1b536758ca68f
prettyturtle <unifconti@gmail.com>
2022-08-27 19:33:38 +0900

    백준_시험 감독 문제풀이

--- AlgorithmSwift/푼 문제들/시험 감독.swift
+++ AlgorithmSwift/푼 문제들/시험 감독.swift
@@ -1,8 +1,33 @@
-//
-//  시험 감독.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/08/25.
-//
+extension String {
+    var asInt: Int { Int(self)! }
+    var asIntArr: [Int] {
+        self
+            .split(separator: " ")
+            .map { Int($0)! }
+    }
+}
 
-import Foundation
+let n = readLine()!.asInt
+var Ai = readLine()!.asIntArr
+
+let bc = readLine()!.asIntArr
+let (b, c) = (bc[0], bc[1])
+
+
+
+Ai = Ai.map { $0 - b }.map { $0 < 0 ? 0 : $0 }
+
+var result = n
+
+for i in Ai {
+    if i == 0 {
+        continue
+    }
+    if i % c == 0 {
+        result += i / c
+    } else {
+        result += i / c + 1
+    }
+}
+
+print(result)
