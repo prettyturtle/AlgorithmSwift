996a1a99b366bf37a3ae03a93628fbbcb65cba92
prettyturtle <unifconti@gmail.com>
2022-09-23 01:01:01 +0900

    백준_방학숙제 문제풀이

--- AlgorithmSwift/푼 문제들/방학숙제.swift
+++ AlgorithmSwift/푼 문제들/방학숙제.swift
@@ -1,8 +1,28 @@
-//
-//  방학숙제.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/09/20.
-//
+extension String {
+    var asInt: Int { Int(self)! }
+    var asIntArr: [Int] {
+        self
+            .split(separator: " ")
+            .map { Int($0)! }
+    }
+}
 
-import Foundation
+let l = readLine()!.asInt
+let a = readLine()!.asInt
+let b = readLine()!.asInt
+let c = readLine()!.asInt
+let d = readLine()!.asInt
+
+
+var aCount = 0
+var bCount = 0
+
+while aCount * c < a {
+    aCount += 1
+}
+
+while bCount * d < b {
+    bCount += 1
+}
+
+print(l - max(aCount, bCount))
