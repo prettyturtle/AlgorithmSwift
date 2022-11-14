541b8871c6a6a1715586e1f02bdd679959748273
prettyturtle <unifconti@gmail.com>
2022-11-15 06:59:09 +0900

    백준_2007 문제풀이

--- AlgorithmSwift/푼 문제들/2007.swift
+++ AlgorithmSwift/푼 문제들/2007.swift
@@ -1,8 +1,38 @@
-//
-//  2007.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/11/13.
-//
+extension String {
+    var asInt: Int { Int(self)! }
+    var asIntArr: [Int] {
+        self
+            .split(separator: " ")
+            .map { Int($0)! }
+    }
+}
 
-import Foundation
+let xy = readLine()!.asIntArr
+let (x, y) = (xy[0], xy[1])
+
+enum Yoil: CaseIterable {
+    case MON
+    case TUE
+    case WED
+    case THU
+    case FRI
+    case SAT
+    case SUN
+}
+
+
+let month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+var sum = 0
+
+for i in 0..<x {
+    sum += month[i]
+}
+
+var cur = Yoil.MON
+
+for i in 0..<sum + y {
+    cur = Yoil.allCases[i % Yoil.allCases.count]
+}
+
+print(cur)
