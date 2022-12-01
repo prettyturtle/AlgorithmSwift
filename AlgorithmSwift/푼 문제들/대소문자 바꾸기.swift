bf2a30dc8e7b2f654ecb7e11f571eaf778122189
prettyturtle <unifconti@gmail.com>
2022-12-02 00:25:37 +0900

    백준_대소문자 바꾸기 문제풀이

--- AlgorithmSwift/푼 문제들/대소문자 바꾸기.swift
+++ AlgorithmSwift/푼 문제들/대소문자 바꾸기.swift
@@ -1,8 +1,25 @@
-//
-//  대소문자 바꾸기.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/12/01.
-//
+let arr1 = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "l", "k", "j", "h", "g", "f", "d", "s", "a", "z", "x", "c", "v", "b", "n", "m"]
+let arr2 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "L", "K", "J", "H", "G", "F", "D", "S", "A", "Z", "X", "C", "V", "B", "N", "M"]
 
-import Foundation
+var dic1 = [String: String]()
+var dic2 = [String: String]()
+
+for i in 0..<arr1.count {
+    dic1[arr1[i]] = arr2[i]
+    dic2[arr2[i]] = arr1[i]
+}
+
+var s = readLine()!.map { String($0) }
+
+for i in 0..<s.count {
+    if let so = dic1[s[i]] {
+        s[i] = so
+        continue
+    }
+    if let de = dic2[s[i]] {
+        s[i] = de
+        continue
+    }
+}
+
+print(s.joined())
