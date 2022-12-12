822c63a39d3dff8b26d324a8e7dedef5f3fc1180
prettyturtle <unifconti@gmail.com>
2022-12-13 00:05:08 +0900

    프로그래머스_옹알이 (2) 문제풀이

--- AlgorithmSwift/푼 문제들/프로그래머스_옹알이 (2).swift
+++ AlgorithmSwift/푼 문제들/프로그래머스_옹알이 (2).swift
@@ -1,8 +1,42 @@
-//
-//  프로그래머스_옹알이 (2).swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/12/12.
-//
-
 import Foundation
+
+func solution(_ babbling: [String]) -> Int {
+    let aa = [
+        "aya": "*",
+        "ye": "@",
+        "woo": "!",
+        "ma": "$"
+    ]
+    
+    var babbling = babbling
+    
+    for i in 0..<babbling.count {        
+        for (k, v) in aa {
+            if babbling[i].contains(k) {
+                babbling[i] = babbling[i].replacingOccurrences(of: k, with: v)
+            }
+        }
+    }
+    
+    func isR(_ s: String) -> Bool {
+        var cur = ""
+        for i in s {
+            if cur == String(i) {
+                return false
+            } else {
+                cur = String(i)
+            }
+        }
+        return true
+    }
+    
+    var answer = 0
+    
+    babbling = babbling.filter { $0 == $0.uppercased() }
+    
+    for i in babbling {
+        if isR(i) { answer += 1 }
+    }
+    
+    return answer
+}
