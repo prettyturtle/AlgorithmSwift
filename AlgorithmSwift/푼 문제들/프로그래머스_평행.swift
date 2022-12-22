9823fe592829ab4f80c979ee84439495003115ca
prettyturtle <unifconti@gmail.com>
2022-12-23 00:13:22 +0900

    프로그래머스_평행 문제풀이

--- AlgorithmSwift/푼 문제들/프로그래머스_평행.swift
+++ AlgorithmSwift/푼 문제들/프로그래머스_평행.swift
@@ -1,8 +1,26 @@
-//
-//  프로그래머스_평행.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2022/12/21.
-//
-
 import Foundation
+
+func solution(_ dots: [[Int]]) -> Int {
+    
+    let nums = [0, 1, 2, 3]
+    
+    for i in 0...2 {
+        for j in i+1...3 {
+            let temp = nums.filter { $0 != i && $0 != j }
+            
+            let a = dots[i]
+            let b = dots[j]
+            let c = dots[temp[0]]
+            let d = dots[temp[1]]
+            
+            let x = Double(b[1] - a[1]) / Double(b[0] - a[0])
+            let y = Double(d[1] - c[1]) / Double(d[0] - c[0])
+            
+            if x == y {
+                return 1
+            }
+        }
+    }
+    
+    return 0
+}
