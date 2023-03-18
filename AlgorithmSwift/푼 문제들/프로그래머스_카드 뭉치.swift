ef66ef337e77930a33df54947c0760b14d2132ea
prettyturtle <unifconti@gmail.com>
2023-03-18 09:15:15 +0900

    프로그래머스_카드 뭉치 문제풀이

--- AlgorithmSwift/푼 문제들/프로그래머스_카드 뭉치.swift
+++ AlgorithmSwift/푼 문제들/프로그래머스_카드 뭉치.swift
@@ -1,8 +1,39 @@
-//
-//  프로그래머스_카드 뭉치.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2023/03/15.
-//
-
 import Foundation
+
+func solution(_ cards1: [String], _ cards2: [String], _ goal: [String]) -> String {
+    
+    var idx1 = 0
+    var idx2 = 0
+    
+    for gol in goal {
+        if idx1 < cards1.count && idx2 < cards2.count {
+            if cards1[idx1] == gol {
+                idx1 += 1
+                continue
+            } else if cards2[idx2] == gol {
+                idx2 += 1
+                continue
+            } else {
+                return "No"
+            }
+        } else if idx1 == cards1.count {
+            if cards2[idx2] == gol {
+                idx2 += 1
+                continue
+            } else {
+                return "No"
+            }
+        } else if idx2 == cards2.count {
+            if cards1[idx1] == gol {
+                idx1 += 1
+                continue
+            } else {
+                return "No"
+            }
+        }
+        
+        
+    }
+    
+    return "Yes"
+}
