6eb88b17169b2b3f3124d9b05a6b24202e3a6682
prettyturtle <unifconti@gmail.com>
2023-03-19 23:54:19 +0900

    프로그래머스_대충 만든 자판 문제풀이

--- AlgorithmSwift/푼 문제들/프로그래머스_대충 만든 자판.swift
+++ AlgorithmSwift/푼 문제들/프로그래머스_대충 만든 자판.swift
@@ -1,8 +1,38 @@
-//
-//  프로그래머스_대충 만든 자판.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2023/03/15.
-//
-
 import Foundation
+
+func solution(_ keymap: [String], _ targets: [String]) -> [Int] {
+    
+    var dic = [String: Int]()
+    
+    for m in keymap {
+        let temp = m.map { String($0) }
+        
+        for i in 0..<temp.count {
+            let co = i + 1
+            
+            if dic[temp[i]] != nil {
+                dic[temp[i]]! = min(dic[temp[i]]!, co)
+            } else {
+                dic[temp[i]] = co
+            }
+        }
+        
+        
+    }
+    
+    return targets.map { target -> Int in
+        let temp = target.map { String($0) }
+        
+        var sum = 0
+        
+        for i in 0..<temp.count {
+            if dic[temp[i]] == nil {
+                return -1
+            } else {
+                sum += dic[temp[i]]!
+            }
+        }
+        
+        return sum
+    }
+}
