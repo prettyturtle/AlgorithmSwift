a244a3280542517c7976b402d574e7afa7038726
prettyturtle <unifconti@gmail.com>
2023-03-20 13:15:52 +0900

    프로그래머스_둘만의 암호 문제풀이

--- AlgorithmSwift/푼 문제들/프로그래머스_둘만의 암호.swift
+++ AlgorithmSwift/푼 문제들/프로그래머스_둘만의 암호.swift
@@ -1,8 +1,34 @@
-//
-//  프로그래머스_둘만의 암호.swift
-//  AlgorithmSwift
-//
-//  Created by yc on 2023/03/15.
-//
-
 import Foundation
+
+func solution(_ s: String, _ skip: String, _ index: Int) -> String {
+    
+    let initalAlphabet = Unicode.Scalar("A").value
+    let endAlphabet = Unicode.Scalar("Z").value
+    
+    
+    var li = [String]()
+    for k in initalAlphabet...endAlphabet {
+       if let val = Unicode.Scalar(k) {
+          li.append(String(val).lowercased())
+       }
+    }
+    
+    li = li.filter { !skip.contains($0)}
+    
+    var result = [String]()
+    
+    for i in s.map { String($0) } {
+        var idx = 0
+        
+        for l in 0..<li.count {
+            if li[l] == i {
+                idx = l
+                break
+            }
+        }
+        
+        result.append(li[(idx + index) % li.count])
+    }
+    
+    return result.joined()
+}
